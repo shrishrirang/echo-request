f227bbb883a6792155433d910815ae3ff70ab01b
Shrirang Shirodkar <shrirs@microsoft.com>
2019-01-11 16:03:42 -0800

    Basic

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express')
+var cors = require('cors')
+var app = express()
+var fs = require('fs')
+
+app.use(cors())
+
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
+
+app.post('/call/postwithoutbody/:id', function (req, res) {
+    var headers = JSON.stringify(req.headers);
+
+    console.log(headers)
+    
+    try {
+        fs.appendFileSync('d:\\home\\out.txt', headers + '\n', { mode: 0o755 });
+    } catch(err) {
+        // An error occurred
+        console.error(err);
+    }
+
+    res.send(headers)
+  })
+  
+app.listen(process.env.PORT || 8088);
