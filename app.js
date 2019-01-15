bb4ffad7ce7ce9e50d4109727af8337d9d870424
Shrirang Shirodkar <shrirs@microsoft.com>
2019-01-15 15:00:33 -0800

    Generalize the routes for both GET and POST

--- app.js
+++ app.js
@@ -5,25 +5,26 @@ var fs = require('fs')
 
 app.use(cors())
 
-
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', function (req, res) {
-  res.send('hello world')
-})
-
-app.post('/call/postwithoutbody/:id', function (req, res) {
-    var headers = JSON.stringify(req.headers) + '\n';
+function respondToRequest(req, res) {
+    var headers = 'Headers received: ' + JSON.stringify(req.headers) + '\n';
 
     console.log(headers)
     
     try {
         fs.appendFileSync('d:\\home\\out.txt', headers);
     } catch(err) {
-        // An error occurred
         console.error(err);
     }
 
     res.send(headers)
-  })
+}
+
+app.get('/*', function (req, res) {
+    respondToRequest(req, res);
+})
+  
+app.post('/*', function (req, res) {
+    respondToRequest(req, res);
+})
   
 app.listen(process.env.PORT || 8088);
